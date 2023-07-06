9cd5aa491ad12543f256c23a629f1a5f3bdca81f
jeegoraf <ferenzlist90@gmail.com>
2023-07-06 13:40:02 +0300

    chore: configure color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: 'd2d0cc',
+        lightBlue: '#2e6d9c',
+        blue: '0c5274',
+        darkBlue: '2c4464',
+        brown: '9c6c4c',
+        beige: 'e1c9b4',
+      },
+    },
   },
   plugins: [],
 }
