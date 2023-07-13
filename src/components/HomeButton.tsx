abd39a436ccc5a537c033743cbed8d463cbd036d
jeegoraf <ferenzlist90@gmail.com>
2023-07-13 21:32:42 +0300

    feat: setup connection between search panel and API

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 
 export function HomeButton(): JSX.Element {
   return (
-    <button className="text-6xl">
-      <Link to="/"> IMDB </Link>
+    <button className="text-6xl text-beige">
+      <Link to="/"> KINOPOISK </Link>
     </button>
   )
 }
