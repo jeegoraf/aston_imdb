90b43ef500908c08d3948a4b0d5a6593b58a8746
jeegoraf <ferenzlist90@gmail.com>
2023-07-15 13:52:23 +0300

    feat: almost finished search panel

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 import { useLazyGetFilmsByKeywordQuery } from '../api'
@@ -6,68 +6,35 @@ import { useLazyGetFilmsByKeywordQuery } from '../api'
 interface Film {
   id: number
   name: string
-  shortDescription: string
-  description: string
-  year: number
-  rating: number
-  poster: string | null
-}
-
-interface FilmRu {
-  id: number
-  name: string
   description: string
   year: number
 }
 
-export function SearchPanel(): JSX.Element {
-  const [keyWord, setKeyWord] = useState('')
-  const [items, setItems] = useState<FilmRu[]>([])
+export function SearchPanel() {
+  const [items, setItems] = useState<Film[]>([])
+  const [getFilmsByKeyword] = useLazyGetFilmsByKeywordQuery()
 
-  const [getFilmsByKeyword, results] = useLazyGetFilmsByKeywordQuery()
-
-  useEffect(() => {
-    const copy = Object.assign([], items)
+  const handleOnSearch = useCallback((keyWord: string) => {
+    const copy: Film[] = []
     getFilmsByKeyword(keyWord).then((response) => {
-      response.data.films.forEach((item: any) => {
-        const film: FilmRu = {
-          id: item.filmId,
-          name: item.nameRu,
+      response.data.docs.forEach((item: any) => {
+        const film: Film = {
+          id: item.id,
+          name: item.name,
           description: item.description,
           year: item.year
         }
         copy.push(film)
       })
       setItems(copy)
-    }).catch((err) => { console.error(err) })
+    }).catch((err) => { alert(err.message) })
+  }, [getFilmsByKeyword])
 
-    // заполнение массива будет происходить в onSearch
-    // самому дебаунс писать не нужно!!
-    // data.docs.forEach((item: any) => {
-    //   const film: Film = {
-    //     id: item.id,
-    //     name: item.name,
-    //     shortDescription: item.shortDescription,
-    //     description: item.description,
-    //     year: item.year,
-    //     rating: item.rating,
-    //     poster: item.poster
-    //   }
-    //   films.push(item)
-    // })
-    // setItems(films)
-  }, [keyWord])
-
-  const handleOnSearch = (keyWord: string) => {
-    setKeyWord(keyWord)
-    console.log(items)
-  }
-
-  const formatResult = (item: FilmRu) => {
+  const formatResult = (item: Film) => {
     return (
       <div className='flex justify-between pr-5'>
         <div>{item.name}</div>
-        <div>{item.description?.slice(0, 180).concat('...') || 'Описание недоступно'}</div>
+        <div>{item.description?.slice(0, 18).concat('...') || 'Описание недоступно'}</div>
         <div>{item.year}</div>
       </div>
     )
