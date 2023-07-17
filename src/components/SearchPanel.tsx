27e5d678ba75b80bc1b26788dee8b5ada495a6d1
jeegoraf <ferenzlist90@gmail.com>
2023-07-17 14:24:25 +0300

    refactor: refactor code after review

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 import { useLazyGetFilmsByKeywordQuery } from '../api'
@@ -14,27 +14,28 @@ export function SearchPanel() {
   const [items, setItems] = useState<Film[]>([])
   const [getFilmsByKeyword] = useLazyGetFilmsByKeywordQuery()
 
-  const handleOnSearch = useCallback((keyWord: string) => {
-    const copy: Film[] = []
+  const handleOnSearch = (keyWord: string) => {
     getFilmsByKeyword(keyWord).then((response) => {
-      response.data.docs.forEach((item: any) => {
+      const res = response.data.docs.map((item: any) => {
         const film: Film = {
           id: item.id,
           name: item.name,
           description: item.description,
           year: item.year
         }
-        copy.push(film)
+        return film
       })
-      setItems(copy)
+      setItems(res)
     }).catch((err) => { alert(err.message) })
-  }, [getFilmsByKeyword])
+  }
 
   const formatResult = (item: Film) => {
+    const description = item.description ? item.description.slice(0, 18).concat('...') : 'Описание недоступно'
+
     return (
       <div className='flex justify-between pr-5'>
         <div>{item.name}</div>
-        <div>{item.description?.slice(0, 18).concat('...') || 'Описание недоступно'}</div>
+        <div>{description}</div>
         <div>{item.year}</div>
       </div>
     )
