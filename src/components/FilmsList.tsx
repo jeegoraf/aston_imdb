676a1063bbccb085089b3c0d44c4fe9735e9be50
jeegoraf <ferenzlist90@gmail.com>
2023-07-19 18:30:21 +0300

    feat: add loading- and not-found-pages

--- src/components/FilmsList.tsx
+++ src/components/FilmsList.tsx
@@ -5,5 +5,7 @@ export function FilmsList(props: { films: Response }) {
   const films = props.films.docs?.map((film) => {
     return <FilmCard key={film.id} name={film.name} id={film.id} description={film.description} year={film.year} poster={film.poster}></FilmCard>
   })
-  return <div className='grid gap-5 grid-cols-4 pt-10'>{films}</div>
+  return films?.length
+    ? <div className='grid gap-5 grid-cols-4 pt-10'>{films}</div>
+    : <div className='flex flex-col justify-center items-center text-4xl text-gray pt-[20vh]'>Nothing found :( </div>
 }
