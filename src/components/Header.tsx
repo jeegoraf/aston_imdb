993d1b1319c9da0cf2f3c4fe8067c8e19f6b832f
jeegoraf <ferenzlist90@gmail.com>
2023-07-19 19:44:30 +0300

    refactor: add log out from server

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,26 @@
+import { getAuth, getIdToken, onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+
 import { useAuth } from '../hooks/useAuth'
 import { HomeButton } from './HomeButton'
 import { SignInAndRegister } from './SignInAndRegister'
 import { SignOutButton } from './SignOutButton'
 
 export function Header(): JSX.Element {
-  const { isAuth } = useAuth()
+  // локальные данные о пользователе
+  const user = useAuth()
 
-  return isAuth
+  // даннные о пользователе с сервера
+  const auth = getAuth()
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log('SIGNED IN')
+    } else {
+      console.log('SIGNED OUT')
+    }
+  })
+
+  return user.isAuth
     ? (
     <div className="sticky flex justify-between bg-blue px-40">
       <HomeButton />
