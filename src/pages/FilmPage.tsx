1aeae4e39ec2aee0e3261912cd9005e513add201
jeegoraf <ferenzlist90@gmail.com>
2023-07-19 20:55:59 +0300

    feat: create new api-enpdoint; create filmpage

--- src/pages/FilmPage.tsx
+++ src/pages/FilmPage.tsx
@@ -1,8 +1,34 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { useGetFilmByIdQuery } from '../api'
 import { Header } from '../components/Header'
 import { SearchPanel } from '../components/SearchPanel'
+import { LoadingPage } from './LoadingPage'
 
 export function FilmPage() {
-  return (
+  const { id } = useParams()
+
+  const definedId = id ?? ''
+
+  const { data, error, isLoading } = useGetFilmByIdQuery(definedId)
+
+  if (isLoading) return <LoadingPage />
+
+  return data ? (
+    <div className="flex flex-col">
+      <Header />
+      <SearchPanel />
+      <div className="flex mt-5 mx-40 bg-gray justify-around">
+        <img src={data?.poster?.url} alt="Изображение недоступно"></img>
+        <div className="flex flex-col justify-between">
+          <span>{`Название: ${data.name}`}</span>
+          <div>{`Описание: ${data.description}`}</div>
+          <div>{`Год: ${data.year}`}</div>
+        </div>
+      </div>
+    </div>
+  ) : (
     <div className="flex flex-col">
       <Header />
       <SearchPanel />
