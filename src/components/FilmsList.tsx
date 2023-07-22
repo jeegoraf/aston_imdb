c96592be0e9fe5e9c6870690ae0868b0fe2c5a1e
jeegoraf <ferenzlist90@gmail.com>
2023-07-23 01:18:23 +0300

    feat: add error boundary to FilmsList

--- src/components/FilmsList.tsx
+++ src/components/FilmsList.tsx
@@ -1,8 +1,10 @@
+import { ErrorBoundary } from 'react-error-boundary'
+
 import { type FilmShort, type Response } from '../types/types'
 import { FilmCard } from './FilmCard'
 
 export function FilmsList(props: { films: Response | { docs: FilmShort[] } }) {
-  const films = props.films.docs?.map((film) => {
+  const films = props.films?.docs?.map((film) => {
     return (
       <FilmCard
         key={film.id}
@@ -14,11 +16,15 @@ export function FilmsList(props: { films: Response | { docs: FilmShort[] } }) {
       ></FilmCard>
     )
   })
-  return films?.length ? (
-    <div className="grid gap-5 grid-cols-4 pt-10">{films}</div>
-  ) : (
-    <div className="flex flex-col justify-center items-center text-4xl text-gray pt-[20vh]">
-      Nothing found :({' '}
-    </div>
+  return (
+    <ErrorBoundary
+      fallback={
+        <div className="flex flex-col justify-center items-center text-4xl text-gray pt-[20vh]">
+          Nothing found :(
+        </div>
+      }
+    >
+      <div className="grid gap-5 grid-cols-4 pt-10">{films}</div>
+    </ErrorBoundary>
   )
 }
